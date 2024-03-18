fbe29fba8928e94538d9817bf74e552b4abc9904
jeenakarmi <jinanakarmi@gmail.com>
2024-03-18 22:17:43 +0545

    Displaying Contents Of A Single Note

--- notes/urls.py
+++ notes/urls.py
@@ -3,4 +3,5 @@ from . import views # for views
 
 urlpatterns = [
     path('notes/', views.list),
+    path('notes/<int:pk>/', views.detail),
 ]
--- notes/views.py
+++ notes/views.py
@@ -6,3 +6,11 @@ from .models import Notes #importing models that is created
 def list(request):
     all_notes = Notes.objects.all() #gets all notes 
     return render(request, 'notes/notes_list.html', {'notes': all_notes}) # renders created destination to notes template # defines notes
+
+
+#Displaying Contents Of A Single Note
+def detail(request, pk): #pk -- primary key of db
+
+    #note is for single
+    note = Notes.objects.get(pk=pk)  #pk is set as default
+    return render(request, 'notes/notes_detail.html', {'note': note}) #specifying name as note to variable note
