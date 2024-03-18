170f507c8c1afad2b4634480e37f709fed20b11d
jeenakarmi <jinanakarmi@gmail.com>
2024-03-18 20:10:49 +0545

    Comments added

--- notes/admin.py
+++ notes/admin.py
@@ -4,6 +4,6 @@ from . import models
 # Register your models here.
 class NotesAdmin(admin.ModelAdmin):
 
-    list_display = ('title',)
+    list_display = ('title',)  #for notes title detail (not anonymous)
 
-admin.site.register(models.Notes, NotesAdmin)
+admin.site.register(models.Notes, NotesAdmin) #registering models.Notes and NotesAdmin
--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Notes(models.Model):
+class Notes(models.Model):      #for tables of database
     title = models.CharField(max_length = 200)
-    text = models.TextField()
+    text = models.TextField()       #unlimited character option textfield()
     created = models.DateTimeField(auto_now_add = True)
