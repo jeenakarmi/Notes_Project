1062c9385c5bdf5164c11ac08ca6561ee4a2780d
jeenakarmi <jinanakarmi@gmail.com>
2024-03-19 21:59:41 +0545

    Detail View With CBV

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,5 @@ from . import views # for views
 
 urlpatterns = [
     path('notes/', views.NotesListView.as_view()),
-    path('notes/<int:pk>/', views.detail),
+    path('notes/<int:pk>/', views.NotesDetailView.as_view()),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Notes #importing models that is created
 
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
 
@@ -15,10 +15,15 @@ class NotesListView(ListView):
 #     all_notes = Notes.objects.all() #gets all notes 
 #     return render(request, 'notes/notes_list.html', {'notes': all_notes}) # renders created destination to notes template # defines notes
 
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "notes"
 
-#Displaying Contents Of A Single Note
-def detail(request, pk): #pk -- primary key of db
 
-    #note is for single
-    note = Notes.objects.get(pk=pk)  #pk is set as default
-    return render(request, 'notes/notes_detail.html', {'note': note}) #specifying name as note to variable note
+# replaced by NotesDetailView
+# #Displaying Contents Of A Single Note
+# def detail(request, pk): #pk -- primary key of db
+
+#     #note is for single
+#     note = Notes.objects.get(pk=pk)  #pk is set as default
+#     return render(request, 'notes/notes_detail.html', {'note': note}) #specifying name as note to variable note
