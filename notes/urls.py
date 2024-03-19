df195becd991aff22d5d78cfa616371c917b142f
jeenakarmi <jinanakarmi@gmail.com>
2024-03-19 21:41:20 +0545

    List View With Class Based Views

--- notes/urls.py
+++ notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views # for views 
 
 urlpatterns = [
-    path('notes/', views.list),
+    path('notes/', views.NotesListView.as_view()),
     path('notes/<int:pk>/', views.detail),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Notes #importing models that is created
 
+from django.views.generic import ListView
+
 # Create your views here.
-    #lists all notes
-def list(request):
-    all_notes = Notes.objects.all() #gets all notes 
-    return render(request, 'notes/notes_list.html', {'notes': all_notes}) # renders created destination to notes template # defines notes
+
+class NotesListView(ListView):
+    model = Notes 
+    context_object_name = "notes"
+
+#replace by NotesListView class
+#     #lists all notes
+# def list(request):
+#     all_notes = Notes.objects.all() #gets all notes 
+#     return render(request, 'notes/notes_list.html', {'notes': all_notes}) # renders created destination to notes template # defines notes
 
 
 #Displaying Contents Of A Single Note
