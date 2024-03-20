103c1522d06de8794f469c4d8121e10d5bf0c27a
jeenakarmi <jinanakarmi@gmail.com>
2024-03-20 15:42:51 +0545

    Base Template set up

--- Notes_Project/settings.py
+++ Notes_Project/settings.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'Notes_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates' 
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
