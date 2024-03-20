93915beaa3247d8d0f9ae9e1f61370be2c82302b
jeenakarmi <jinanakarmi@gmail.com>
2024-03-20 16:49:17 +0545

    Code got chatty

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name = 'home'),
+    path('', views.HomeView.as_view(), name = 'home'), #for homepage
     path('authorized/',views.AuthorizedViews.as_view(), name='authorized'), # for class based views for authentication
 ]
