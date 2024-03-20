57913bf82c867f47440b3d0c1644c292667db618
jeenakarmi <jinanakarmi@gmail.com>
2024-03-20 18:43:26 +0545

    Adding URLs To Each Note In Notes List

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,5 @@ from . import views # for views
 
 urlpatterns = [
     path('notes/', views.NotesListView.as_view(), name= "notes.list"),
-    path('notes/<int:pk>/', views.NotesDetailView.as_view()),
+    path('notes/<int:pk>/', views.NotesDetailView.as_view(), name = "notes.detail"),
 ]
