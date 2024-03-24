eb3ec353b47e95f3cfe0b4c8ff0656281108cfcb
jeenakarmi <jinanakarmi@gmail.com>
2024-03-24 11:31:33 +0545

    Styling error msg

--- notes/forms.py
+++ notes/forms.py
@@ -5,12 +5,13 @@ class NotesForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('title' , 'text')
-
-    #only for django notes 
+        
 '''
+    #only for django notes 
     def clean_title(self):
         title = self.cleaned_data['title']
         if 'Django' not in title:
             raise forms.ValidationError('Notes Should be about Django only! ')
         return title
-'''
+
+'''   
