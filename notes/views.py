5e5be19356af5dc9a4ac7a6e8bd22346e543d50e
jeenakarmi <jinanakarmi@gmail.com>
2024-03-24 09:10:07 +0545

    Creating notes with django generic views

--- notes/views.py
+++ notes/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render
 from .models import Notes #importing models that is created
 
-from django.views.generic import ListView,DetailView
+from django.views.generic import CreateView, ListView ,DetailView
 
 # Create your views here.
+class NotesCreateView(CreateView):
+    model = Notes
+    fields = ['title', 'text']  # notes contain title and content text only
+
+    #after successful creation of  new note, allows user to go  -- all list of notes
+    success_url = '/smart/notes/'
+
+
 
 class NotesListView(ListView):
     model = Notes 
     context_object_name = "notes"
 
 #replace by NotesListView class
-#     #lists all notes
+''' 
+    #lists all notes
 # def list(request):
 #     all_notes = Notes.objects.all() #gets all notes 
 #     return render(request, 'notes/notes_list.html', {'notes': all_notes}) # renders created destination to notes template # defines notes
+'''
 
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "notes"
 
-
 # replaced by NotesDetailView
-# #Displaying Contents Of A Single Note
+'''
+     #Displaying Contents Of A Single Note
 # def detail(request, pk): #pk -- primary key of db
 
 #     #note is for single
 #     note = Notes.objects.get(pk=pk)  #pk is set as default
 #     return render(request, 'notes/notes_detail.html', {'note': note}) #specifying name as note to variable note
+'''
