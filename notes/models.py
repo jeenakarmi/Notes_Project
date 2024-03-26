d6765a30559d507ec8284ee50b1ac231ee31e44d
jeenakarmi <jinanakarmi@gmail.com>
2024-03-26 10:45:07 +0545

    Added Users To Notes Models

--- notes/models.py
+++ notes/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Notes(models.Model):      #for tables of database
     title = models.CharField(max_length = 200)
     text = models.TextField()       #unlimited character option textfield()
     created = models.DateTimeField(auto_now_add = True)
+    user = models.ForeignKey(User, on_delete =models.CASCADE, related_name = 'notes') #to delete all notes id user is deleted
+    
