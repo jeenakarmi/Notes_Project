47b9e5e6ed8fe8d2f026a6f8c10836203094bb37
jeenakarmi <jinanakarmi@gmail.com>
2024-03-29 16:32:19 +0545

    No signup for current users; only logout

--- home/views.py
+++ home/views.py
@@ -16,6 +16,11 @@ class SignupView(CreateView):
     template_name = 'home/register.html'
     success_url = 'smart/notes'
 
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated :
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
 
